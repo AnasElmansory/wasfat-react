c26a958c2f8db45728a0ff69d980c9867c104438
AnasElmansory <ansmg5555@gmail.com>
2021-09-14 02:48:37 +0200

    Delete src directory

--- src/firebase/helper.ts
+++
@@ -1,9 +0,0 @@
-import { firestore, store } from "./client";
-export default async function isAdmin(email: string): Promise<boolean> {
-  const adminQuery = firestore.query(
-    firestore.collection(store, "admins"),
-    firestore.where("email", "==", email)
-  );
-  const admin = await firestore.getDocs(adminQuery);
-  return !admin.empty;
-}
